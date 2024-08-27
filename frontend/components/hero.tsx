578bc9db5601dc67612625bd3782cce11aff5f5b
youssef elhoubi <121304134+youssefggmg@users.noreply.github.com>
2024-08-27 01:49:41 +0100

    the start of the last episode the t4s

---
+++ frontend/components/hero.tsx
@@ -0,0 +1,42 @@
+import VideoThumb from '@/public/images/hero-image-01.jpg'
+import ModalVideo from '@/components/modal-video'
+
+export default function Hero() {
+  return (
+    <section>
+      <div className="max-w-6xl mx-auto px-4 sm:px-6 relative">
+
+        {/* Illustration behind hero content */}
+
+        {/* Hero content */}
+        <div className="relative pt-32 pb-10 md:pt-40 md:pb-16">
+
+          {/* Section header */}
+          <div className="max-w-3xl mx-auto text-center pb-12 md:pb-16">
+            <h1 className="h1 mb-4" data-aos="fade-up">Landing template for startups</h1>
+            <p className="text-xl text-gray-400 mb-8" data-aos="fade-up" data-aos-delay="200">Our landing page template works on all devices, so you only have to set it up once, and get beautiful results forever.</p>
+            <div className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center">
+              <div data-aos="fade-up" data-aos-delay="400">
+                <a className="btn text-white bg-purple-600 hover:bg-purple-700 w-full mb-4 sm:w-auto sm:mb-0" href="#0">Start free trial</a>
+              </div>
+              <div data-aos="fade-up" data-aos-delay="600">
+                <a className="btn text-white bg-gray-700 hover:bg-gray-800 w-full sm:w-auto sm:ml-4" href="#0">Learn more</a>
+              </div>
+            </div>
+          </div>
+
+          <ModalVideo
+            thumb={VideoThumb}
+            thumbWidth={1024}
+            thumbHeight={576}
+            thumbAlt="Modal video thumbnail"
+            video="/videos/video.mp4"
+            videoWidth={1920}
+            videoHeight={1080} />
+
+        </div>
+
+      </div>
+    </section>
+  )
+}

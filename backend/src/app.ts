6142e79aae5199cb4b2f41fa9f396fa129cf4138
youssef elhoubi <121304134+youssefggmg@users.noreply.github.com>
2024-09-01 02:51:27 +0100

    "Added search router to app"

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import authrouter from "./routers/authrouter";
 import dashboardrouter from "./routers/dashboardrout";
+import searchrout from "./routers/searshrouter";
 dotenv.config()
 
 const port = process.env.port ||3000;
@@ -11,6 +12,7 @@ app.use(express.json());
 
 app.use("/api/auth",authrouter);
 app.use("/api/auth",dashboardrouter);
+app.use("/api/auth",searchrout);
 
 
 app.listen(port,()=>{console.log(`server is running on port ${port}`)});
---
+++ backend/src/routers/searshrouter.ts
@@ -0,0 +1,12 @@
+import express from "express";
+import { searchByCity,searchByProviderName,searchByavailability,searchByserviceName } from "../controllers/searchcontroller";
+
+const searchrout = express.Router()
+
+searchrout.route("search/:name").get(searchByProviderName);
+searchrout.route("search/:city").get(searchByCity);
+searchrout.route("search/:availability").get(searchByavailability);
+searchrout.route("search/:availability").get(searchByserviceName);
+
+export default searchrout
+
